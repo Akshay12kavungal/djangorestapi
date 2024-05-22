557fd674f93de5a9749082ecef5d478cc3d16aa3
Akshay <akshay@nanokernel.net>
2024-05-22 18:12:12 +0530

    doctor table com pleted

--- myproject/admin1/urls.py
+++ myproject/admin1/urls.py
@@ -8,6 +8,7 @@ from .views import (DoctorListView,
                     DepartmentsListView,
 
 )
+from .views import Doctor2ListView, Doctor2DetailView, Doctor2CreateView, Doctor2UpdateView, Doctor2DeleteView
 
 urlpatterns = [
 
@@ -22,6 +23,14 @@ urlpatterns = [
     path('doctors_list/<int:pk>/', DoctorDetailView.as_view(), name='doctor_detail'),
     path('doctors_list/update/<int:pk>/', DoctorUpdateView.as_view(), name='doctor_update'),
     path('doctors_list/delete/<int:pk>/', DoctorDeleteView.as_view(), name='doctor_delete'),
+
+
+
+    path('doctors2/', Doctor2ListView.as_view(), name='doctor2_list'),
+    path('doctors2/<int:pk>/', Doctor2DetailView.as_view(), name='doctor2_detail'),
+    path('doctors2/create/', Doctor2CreateView.as_view(), name='doctor2_create'),
+    path('doctors2/update/<int:pk>/', Doctor2UpdateView.as_view(), name='doctor2_update'),
+    path('doctors2/delete/<int:pk>/', Doctor2DeleteView.as_view(), name='doctor2_delete'),
 
     
 
