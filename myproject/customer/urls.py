9e4cd95bb3a430f6631b45d874634735786d835e
Akshay <akshay@nanokernel.net>
2024-06-05 09:49:25 +0530

    route urls

--- myproject/customer/urls.py
+++ myproject/customer/urls.py
@@ -1,44 +1,8 @@
 from django.urls import path, include
 from . import views
 from .views import login_view
-from customer.api.views import LoginAPI
-
-from rest_framework.routers import DefaultRouter
-from customer.api.views import(
-     DepartmentSerializerViewset,
-     DoctorsSerializerViewset,
-     BookingSerializerViewset,
-     ContactSerializerViewset,
-     ProfileSerializerViewset,
-     UserSerializerViewset,
-     
 
 
-)
-from rest_framework.permissions import AllowAny
-
-
-router = DefaultRouter()
-router.register(r'department-api', DepartmentSerializerViewset, basename='department-router')
-router.register(r'doctor-api', DoctorsSerializerViewset, basename='doctor-router')
-router.register(r'booking-api', BookingSerializerViewset, basename='booking-router')
-router.register(r'contact-api', ContactSerializerViewset, basename='contact-router')
-router.register(r'profile-api', ProfileSerializerViewset, basename='profile-router')
-router.register(r'registeration-api', UserSerializerViewset, basename='registeration-router')
-
-
-
-
-class CustomAPIRootView(DefaultRouter.APIRootView):
-    permission_classes = [AllowAny]
-
-
-router.APIRootView = CustomAPIRootView
-
-router_urls = [
-    path('api/', include(router.urls)),
-    path('api/login-api/', LoginAPI.as_view(), name='login_api'),
-]
 
 urlpatterns = [
 
@@ -55,15 +19,16 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('profile/update/', views.profile_update, name='profile_update'),
     path('my_appointments/', views.my_appointments, name='my_appointments'),
-
-
     path('login/', login_view, name='login'),
-
     path('check_username/', views.check_username, name='check_username'),
+
+    path("api/", include("customer.api.urls")),
+
+    
 ]
 
 
-urlpatterns += router_urls
+
 
 
 
